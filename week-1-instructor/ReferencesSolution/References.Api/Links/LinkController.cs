64bfb83a47e27960261c2620244473e2c664e73a
Magariou Diallo <diallomagariou9@gmail.com>
2025-07-07 16:29:32 -0400

    day 1

--- week-1-instructor/ReferencesSolution/References.Api/Links/LinkController.cs
+++ week-1-instructor/ReferencesSolution/References.Api/Links/LinkController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 
 namespace References.Api.Links;
 
 public class LinkController : ControllerBase
 {
+    // some code here that will get called when a GET /links is sent to this server.
 
     [HttpGet("/links")]
     public async Task<ActionResult> GetAllLinksAsync(CancellationToken token)
@@ -11,4 +13,3 @@ public class LinkController : ControllerBase
         return Ok();
     }
 }
-
---
+++ week-1/ReferencesSolution/References.Tests/AddingNumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace References.Tests;
+
+public class AddingNumbers
+{
+    [Fact]
+    public void CanAddTwoNumbers()
+    {
+        // Given
+        int a = 10;
+        int b = 20;
+        int answer;
+
+        // When
+        answer = a + b;
+
+        // Then
+        // Answer should be 30
+        Assert.Equal(31, answer);
+    }
+}
